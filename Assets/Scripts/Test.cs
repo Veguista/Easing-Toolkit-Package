ce7253abe9ac90e8e729ac961cd0b328c37b68c6
Veguista <66578267+Veguista@users.noreply.github.com>
2024-11-27 15:55:03 +0100

    Minor changes

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,5 +2,15 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
