0d373c9a3239b15693ab1d9ef976799482ca34d3
Veguista <66578267+Veguista@users.noreply.github.com>
2025-01-02 03:49:13 +0100

    Added Asset Store tools Package to the project. Added Namespaces to every Test

--- Assets/Easing Toolkit/Tests/Editor/Tests_SecondOrderClasses.cs
+++ Assets/Easing Toolkit/Tests/Editor/Tests_SecondOrderClasses.cs
@@ -2,95 +2,99 @@ using NUnit.Framework;
 using EasingToolkit.SecondOrderDynamics;
 using UnityEngine;
 
-public class Tests_SecondOrderClasses : MonoBehaviour
+
+namespace EasingToolkit.Tests
 {
-    [Test]
-    public void T_FirstUpdateEqualZero()
+    public class Tests_SecondOrderClasses : MonoBehaviour
     {
-        SecondOrder_1D secondOrder1D = new SecondOrder_1D(1, 1, 0, 0);
-        SecondOrder_2D secondOrder2D = new SecondOrder_2D(1, 1, 0, Vector2.zero);
-        SecondOrder_3D secondOrder3D = new SecondOrder_3D(1, 1, 0, Vector3.zero);
-        SecondOrder_Rotation secondOrderRot = new SecondOrder_Rotation(1, 1, 0, Quaternion.identity);
+        [Test]
+        public void T_FirstUpdateEqualZero()
+        {
+            SecondOrder_1D secondOrder1D = new SecondOrder_1D(1, 1, 0, 0);
+            SecondOrder_2D secondOrder2D = new SecondOrder_2D(1, 1, 0, Vector2.zero);
+            SecondOrder_3D secondOrder3D = new SecondOrder_3D(1, 1, 0, Vector3.zero);
+            SecondOrder_Rotation secondOrderRot = new SecondOrder_Rotation(1, 1, 0, Quaternion.identity);
 
-        Assert.AreEqual(0, secondOrder1D.Update(1.0f, 1));
-        Assert.AreEqual(Vector2.zero, secondOrder2D.Update(1.0f, Vector2.one));
-        Assert.AreEqual(Vector3.zero, secondOrder3D.Update(1.0f, Vector3.one));
-        Assert.AreEqual(Quaternion.identity, secondOrderRot.Update(1.0f, Quaternion.Euler(90, 90, 0)));
-    }
+            Assert.AreEqual(0, secondOrder1D.Update(1.0f, 1));
+            Assert.AreEqual(Vector2.zero, secondOrder2D.Update(1.0f, Vector2.one));
+            Assert.AreEqual(Vector3.zero, secondOrder3D.Update(1.0f, Vector3.one));
+            Assert.AreEqual(Quaternion.identity, secondOrderRot.Update(1.0f, Quaternion.Euler(90, 90, 0)));
+        }
 
-    [Test]
-    public void T_SecondUpdateAfterOneSec()
-    {
-        SecondOrder_1D secondOrder1D = new SecondOrder_1D(1, 1, 0, 0);
-        SecondOrder_2D secondOrder2D = new SecondOrder_2D(1, 1, 0, Vector2.zero);
-        SecondOrder_3D secondOrder3D = new SecondOrder_3D(1, 1, 0, Vector3.zero);
-        SecondOrder_Rotation secondOrderRot = new SecondOrder_Rotation(1, 1, 0, Quaternion.identity);
+        [Test]
+        public void T_SecondUpdateAfterOneSec()
+        {
+            SecondOrder_1D secondOrder1D = new SecondOrder_1D(1, 1, 0, 0);
+            SecondOrder_2D secondOrder2D = new SecondOrder_2D(1, 1, 0, Vector2.zero);
+            SecondOrder_3D secondOrder3D = new SecondOrder_3D(1, 1, 0, Vector3.zero);
+            SecondOrder_Rotation secondOrderRot = new SecondOrder_Rotation(1, 1, 0, Quaternion.identity);
 
-        secondOrder1D.Update(0.1f, 1 * 0.1f);
-        secondOrder2D.Update(0.1f, Vector2.one * 0.1f);
-        secondOrder3D.Update(0.1f, Vector3.one * 0.1f);
-        secondOrderRot.Update(0.1f, Quaternion.Euler(90 * 0.1f, 90 * 0.1f, 0));
+            secondOrder1D.Update(0.1f, 1 * 0.1f);
+            secondOrder2D.Update(0.1f, Vector2.one * 0.1f);
+            secondOrder3D.Update(0.1f, Vector3.one * 0.1f);
+            secondOrderRot.Update(0.1f, Quaternion.Euler(90 * 0.1f, 90 * 0.1f, 0));
 
-        Assert.AreEqual(0.314159274f, secondOrder1D.Update(1.0f, 1));
-        Assert.AreEqual(new Vector2(0.314159274f, 0.314159274f), secondOrder2D.Update(1.0f, Vector2.one));
-        Assert.AreEqual(new Vector3(0.314159274f, 0.314159274f, 0.314159274f), secondOrder3D.Update(1.0f, Vector3.one));
-        Assert.AreEqual(new Quaternion(0.245726675f, 0.245726675f, -0.0193391107f, 0.980660617f), secondOrderRot.Update(1.0f, Quaternion.Euler(90, 90, 0)));
-    }
+            Assert.AreEqual(0.314159274f, secondOrder1D.Update(1.0f, 1));
+            Assert.AreEqual(new Vector2(0.314159274f, 0.314159274f), secondOrder2D.Update(1.0f, Vector2.one));
+            Assert.AreEqual(new Vector3(0.314159274f, 0.314159274f, 0.314159274f), secondOrder3D.Update(1.0f, Vector3.one));
+            Assert.AreEqual(new Quaternion(0.245726675f, 0.245726675f, -0.0193391107f, 0.980660617f), secondOrderRot.Update(1.0f, Quaternion.Euler(90, 90, 0)));
+        }
 
-    [Test]
-    public void T_SecondUpdateAfterOneSecDifFrequency()
-    {
-        SecondOrder_1D secondOrder1D = new SecondOrder_1D(5, 1, 0, 0);
-        SecondOrder_2D secondOrder2D = new SecondOrder_2D(5, 1, 0, Vector2.zero);
-        SecondOrder_3D secondOrder3D = new SecondOrder_3D(5, 1, 0, Vector3.zero);
-        SecondOrder_Rotation secondOrderRot = new SecondOrder_Rotation(5, 1, 0, Quaternion.identity);
+        [Test]
+        public void T_SecondUpdateAfterOneSecDifFrequency()
+        {
+            SecondOrder_1D secondOrder1D = new SecondOrder_1D(5, 1, 0, 0);
+            SecondOrder_2D secondOrder2D = new SecondOrder_2D(5, 1, 0, Vector2.zero);
+            SecondOrder_3D secondOrder3D = new SecondOrder_3D(5, 1, 0, Vector3.zero);
+            SecondOrder_Rotation secondOrderRot = new SecondOrder_Rotation(5, 1, 0, Quaternion.identity);
 
-        secondOrder1D.Update(0.1f, 1 * 0.1f);
-        secondOrder2D.Update(0.1f, Vector2.one * 0.1f);
-        secondOrder3D.Update(0.1f, Vector3.one * 0.1f);
-        secondOrderRot.Update(0.1f, Quaternion.Euler(90 * 0.1f, 90 * 0.1f, 0));
+            secondOrder1D.Update(0.1f, 1 * 0.1f);
+            secondOrder2D.Update(0.1f, Vector2.one * 0.1f);
+            secondOrder3D.Update(0.1f, Vector3.one * 0.1f);
+            secondOrderRot.Update(0.1f, Quaternion.Euler(90 * 0.1f, 90 * 0.1f, 0));
 
-        Assert.AreEqual(0.915439665f, secondOrder1D.Update(1.0f, 1));
-        Assert.AreEqual(new Vector2(0.915439665f, 0.915439665f), secondOrder2D.Update(1.0f, Vector2.one));
-        Assert.AreEqual(new Vector3(0.915439665f, 0.915439665f, 0.915439665f), secondOrder3D.Update(1.0f, Vector3.one));
-        Assert.AreEqual(new Quaternion(0.716031551f, 0.716031551f, -0.0563529097f, 0.943646371f), secondOrderRot.Update(1.0f, Quaternion.Euler(90, 90, 0)));
-    }
+            Assert.AreEqual(0.915439665f, secondOrder1D.Update(1.0f, 1));
+            Assert.AreEqual(new Vector2(0.915439665f, 0.915439665f), secondOrder2D.Update(1.0f, Vector2.one));
+            Assert.AreEqual(new Vector3(0.915439665f, 0.915439665f, 0.915439665f), secondOrder3D.Update(1.0f, Vector3.one));
+            Assert.AreEqual(new Quaternion(0.716031551f, 0.716031551f, -0.0563529097f, 0.943646371f), secondOrderRot.Update(1.0f, Quaternion.Euler(90, 90, 0)));
+        }
 
-    [Test]
-    public void T_SecondUpdateAfterOneSecDifDampening()
-    {
-        SecondOrder_1D secondOrder1D = new SecondOrder_1D(1, 0.15f, 0, 0);
-        SecondOrder_2D secondOrder2D = new SecondOrder_2D(1, 0.15f, 0, Vector2.zero);
-        SecondOrder_3D secondOrder3D = new SecondOrder_3D(1, 0.15f, 0, Vector3.zero);
-        SecondOrder_Rotation secondOrderRot = new SecondOrder_Rotation(1, 0.15f, 0, Quaternion.identity);
+        [Test]
+        public void T_SecondUpdateAfterOneSecDifDampening()
+        {
+            SecondOrder_1D secondOrder1D = new SecondOrder_1D(1, 0.15f, 0, 0);
+            SecondOrder_2D secondOrder2D = new SecondOrder_2D(1, 0.15f, 0, Vector2.zero);
+            SecondOrder_3D secondOrder3D = new SecondOrder_3D(1, 0.15f, 0, Vector3.zero);
+            SecondOrder_Rotation secondOrderRot = new SecondOrder_Rotation(1, 0.15f, 0, Quaternion.identity);
 
-        secondOrder1D.Update(0.1f, 1 * 0.1f);
-        secondOrder2D.Update(0.1f, Vector2.one * 0.1f);
-        secondOrder3D.Update(0.1f, Vector3.one * 0.1f);
-        secondOrderRot.Update(0.1f, Quaternion.Euler(90 * 0.1f, 90 * 0.1f, 0));
+            secondOrder1D.Update(0.1f, 1 * 0.1f);
+            secondOrder2D.Update(0.1f, Vector2.one * 0.1f);
+            secondOrder3D.Update(0.1f, Vector3.one * 0.1f);
+            secondOrderRot.Update(0.1f, Quaternion.Euler(90 * 0.1f, 90 * 0.1f, 0));
 
-        Assert.AreEqual(0.348132759f, secondOrder1D.Update(1.0f, 1));
-        Assert.AreEqual(new Vector2(0.348132789f, 0.348132789f), secondOrder2D.Update(1.0f, Vector2.one));
-        Assert.AreEqual(new Vector3(0.348132789f, 0.348132789f, 0.348132789f), secondOrder3D.Update(1.0f, Vector3.one));
-        Assert.AreEqual(new Quaternion(0.272299796f, 0.272299796f, -0.0214304626f, 0.978569269f), secondOrderRot.Update(1.0f, Quaternion.Euler(90, 90, 0)));
-    }
+            Assert.AreEqual(0.348132759f, secondOrder1D.Update(1.0f, 1));
+            Assert.AreEqual(new Vector2(0.348132789f, 0.348132789f), secondOrder2D.Update(1.0f, Vector2.one));
+            Assert.AreEqual(new Vector3(0.348132789f, 0.348132789f, 0.348132789f), secondOrder3D.Update(1.0f, Vector3.one));
+            Assert.AreEqual(new Quaternion(0.272299796f, 0.272299796f, -0.0214304626f, 0.978569269f), secondOrderRot.Update(1.0f, Quaternion.Euler(90, 90, 0)));
+        }
 
-    [Test]
-    public void T_SecondUpdateAfterOneSecDifInitialResponse()
-    {
-        SecondOrder_1D secondOrder1D = new SecondOrder_1D(1, 1, -1, 0);
-        SecondOrder_2D secondOrder2D = new SecondOrder_2D(1, 1, -1, Vector2.zero);
-        SecondOrder_3D secondOrder3D = new SecondOrder_3D(1, 1, -1, Vector3.zero);
-        SecondOrder_Rotation secondOrderRot = new SecondOrder_Rotation(1, 1, -1, Quaternion.identity);
+        [Test]
+        public void T_SecondUpdateAfterOneSecDifInitialResponse()
+        {
+            SecondOrder_1D secondOrder1D = new SecondOrder_1D(1, 1, -1, 0);
+            SecondOrder_2D secondOrder2D = new SecondOrder_2D(1, 1, -1, Vector2.zero);
+            SecondOrder_3D secondOrder3D = new SecondOrder_3D(1, 1, -1, Vector3.zero);
+            SecondOrder_Rotation secondOrderRot = new SecondOrder_Rotation(1, 1, -1, Quaternion.identity);
 
-        secondOrder1D.Update(0.1f, 1 * 0.1f);
-        secondOrder2D.Update(0.1f, Vector2.one * 0.1f);
-        secondOrder3D.Update(0.1f, Vector3.one * 0.1f);
-        secondOrderRot.Update(0.1f, Quaternion.Euler(90 * 0.1f, 90 * 0.1f, 0));
+            secondOrder1D.Update(0.1f, 1 * 0.1f);
+            secondOrder2D.Update(0.1f, Vector2.one * 0.1f);
+            secondOrder3D.Update(0.1f, Vector3.one * 0.1f);
+            secondOrderRot.Update(0.1f, Quaternion.Euler(90 * 0.1f, 90 * 0.1f, 0));
 
-        Assert.AreEqual(-0.185840711f, secondOrder1D.Update(1.0f, 1));
-        Assert.AreEqual(new Vector2(-0.185840711f, -0.185840711f), secondOrder2D.Update(1.0f, Vector2.one));
-        Assert.AreEqual(new Vector3(-0.185840711f, -0.185840711f, -0.185840711f), secondOrder3D.Update(1.0f, Vector3.one));
-        Assert.AreEqual(new Quaternion(-0.145359471f, -0.145359471f, 0.0114400387f, 1.01144016f), secondOrderRot.Update(1.0f, Quaternion.Euler(90, 90, 0)));
+            Assert.AreEqual(-0.185840711f, secondOrder1D.Update(1.0f, 1));
+            Assert.AreEqual(new Vector2(-0.185840711f, -0.185840711f), secondOrder2D.Update(1.0f, Vector2.one));
+            Assert.AreEqual(new Vector3(-0.185840711f, -0.185840711f, -0.185840711f), secondOrder3D.Update(1.0f, Vector3.one));
+            Assert.AreEqual(new Quaternion(-0.145359471f, -0.145359471f, 0.0114400387f, 1.01144016f), secondOrderRot.Update(1.0f, Quaternion.Euler(90, 90, 0)));
+        }
     }
 }
--- Assets/Easing Toolkit/Tests/Runtime/Tests_ApplyEase.cs
+++ Assets/Easing Toolkit/Tests/Runtime/Tests_ApplyEase.cs
@@ -1,137 +1,140 @@
 using NUnit.Framework;
 using EasingToolkit;
 
-public class Tests_ApplyEase
+namespace EasingToolkit.Tests
 {
-    [Test]
-    public void T_EasingValueWhen_1()
+    public class Tests_ApplyEase
     {
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InSine));
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.OutSine));
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InOutSine));
-                        
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InQuad));
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.OutQuad));
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InOutQuad));
-                        
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InCubic));
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.OutCubic));
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InOutCubic));
-                        
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InQuart));
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.OutQuart));
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InOutQuart));
-                        
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InQuint));
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.OutQuint));
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InOutQuint));
-                        
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InExpo));
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.OutExpo));
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InOutExpo));
-                        
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InCirc));
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.OutCirc));
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InOutCirc));
-                        
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InBack));
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.OutBack));
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InOutBack));
-                        
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InElastic));
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.OutElastic));
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InOutElastic));
-                        
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InBounce));
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.OutBounce));
-        Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InOutBounce));
-    }
+        [Test]
+        public void T_EasingValueWhen_1()
+        {
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InSine));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.OutSine));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InOutSine));
 
-    [Test]
-    public void T_EasingValueWhen_0()
-    {
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InSine));
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.OutSine));
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InOutSine));
-                                         
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InQuad));
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.OutQuad));
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InOutQuad));
-                                         
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InCubic));
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.OutCubic));
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InOutCubic));
-                                        
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InQuart));
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.OutQuart));
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InOutQuart));
-                                        
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InQuint));
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.OutQuint));
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InOutQuint));
-                                        
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InExpo));
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.OutExpo));
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InOutExpo));
-                                        
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InCirc));
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.OutCirc));
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InOutCirc));
-                                        
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InBack));
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.OutBack));
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InOutBack));
-                                        
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InElastic));
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.OutElastic));
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InOutElastic));
-                                        
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InBounce));
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.OutBounce));
-        Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InOutBounce));
-    }
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InQuad));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.OutQuad));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InOutQuad));
 
-    [Test]
-    public void T_EasingValueWhen_0_5f()
-    {
-        Assert.AreEqual(0.292893231f, Easing.ApplyEase(0.5f, EaseType.InSine));
-        Assert.AreEqual(0.707106769f, Easing.ApplyEase(0.5f, EaseType.OutSine));
-        Assert.AreEqual(0.5f, Easing.ApplyEase(0.5f, EaseType.InOutSine));
-                                          
-        Assert.AreEqual(0.25f, Easing.ApplyEase(0.5f, EaseType.InQuad));
-        Assert.AreEqual(0.75f, Easing.ApplyEase(0.5f, EaseType.OutQuad));
-        Assert.AreEqual(0.5f, Easing.ApplyEase(0.5f, EaseType.InOutQuad));
-                                          
-        Assert.AreEqual(0.125f, Easing.ApplyEase(0.5f, EaseType.InCubic));
-        Assert.AreEqual(0.875f, Easing.ApplyEase(0.5f, EaseType.OutCubic));
-        Assert.AreEqual(0.5f, Easing.ApplyEase(0.5f, EaseType.InOutCubic));
-                                          
-        Assert.AreEqual(0.0625f, Easing.ApplyEase(0.5f, EaseType.InQuart));
-        Assert.AreEqual(0.9375f, Easing.ApplyEase(0.5f, EaseType.OutQuart));
-        Assert.AreEqual(0.5f, Easing.ApplyEase(0.5f, EaseType.InOutQuart));
-                                          
-        Assert.AreEqual(0.03125f, Easing.ApplyEase(0.5f, EaseType.InQuint));
-        Assert.AreEqual(0.96875f, Easing.ApplyEase(0.5f, EaseType.OutQuint));
-        Assert.AreEqual(0.5f, Easing.ApplyEase(0.5f, EaseType.InOutQuint));
-                                          
-        Assert.AreEqual(0.03125f, Easing.ApplyEase(0.5f, EaseType.InExpo));
-        Assert.AreEqual(0.96875f, Easing.ApplyEase(0.5f, EaseType.OutExpo));
-        Assert.AreEqual(0.5f, Easing.ApplyEase(0.5f, EaseType.InOutExpo));
-                                          
-        Assert.AreEqual(0.133974612f, Easing.ApplyEase(0.5f, EaseType.InCirc));
-        Assert.AreEqual(0.8660254f, Easing.ApplyEase(0.5f, EaseType.OutCirc));
-        Assert.AreEqual(0.5f, Easing.ApplyEase(0.5f, EaseType.InOutCirc));
-                                          
-        Assert.AreEqual(-0.087697506f, Easing.ApplyEase(0.5f, EaseType.InBack));
-        Assert.AreEqual(1.08769751f, Easing.ApplyEase(0.5f, EaseType.OutBack));
-        Assert.AreEqual(0.5f, Easing.ApplyEase(0.5f, EaseType.InOutBack));
-                                          
-        Assert.AreEqual(-0.0156249879f, Easing.ApplyEase(0.5f, EaseType.InElastic));
-        Assert.AreEqual(1.015625f, Easing.ApplyEase(0.5f, EaseType.OutElastic));
-        Assert.AreEqual(0.5f, Easing.ApplyEase(0.5f, EaseType.InOutElastic));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InCubic));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.OutCubic));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InOutCubic));
 
-        Assert.AreEqual(0.234375f, Easing.ApplyEase(0.5f, EaseType.InBounce));
-        Assert.AreEqual(0.765625f, Easing.ApplyEase(0.5f, EaseType.OutBounce));
-        Assert.AreEqual(0.5f, Easing.ApplyEase(0.5f, EaseType.InOutBounce));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InQuart));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.OutQuart));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InOutQuart));
+
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InQuint));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.OutQuint));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InOutQuint));
+
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InExpo));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.OutExpo));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InOutExpo));
+
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InCirc));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.OutCirc));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InOutCirc));
+
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InBack));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.OutBack));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InOutBack));
+
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InElastic));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.OutElastic));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InOutElastic));
+
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InBounce));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.OutBounce));
+            Assert.AreEqual(1, Easing.ApplyEase(1, EaseType.InOutBounce));
+        }
+
+        [Test]
+        public void T_EasingValueWhen_0()
+        {
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InSine));
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.OutSine));
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InOutSine));
+
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InQuad));
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.OutQuad));
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InOutQuad));
+
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InCubic));
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.OutCubic));
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InOutCubic));
+
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InQuart));
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.OutQuart));
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InOutQuart));
+
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InQuint));
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.OutQuint));
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InOutQuint));
+
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InExpo));
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.OutExpo));
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InOutExpo));
+
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InCirc));
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.OutCirc));
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InOutCirc));
+
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InBack));
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.OutBack));
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InOutBack));
+
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InElastic));
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.OutElastic));
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InOutElastic));
+
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InBounce));
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.OutBounce));
+            Assert.AreEqual(0, Easing.ApplyEase(0, EaseType.InOutBounce));
+        }
+
+        [Test]
+        public void T_EasingValueWhen_0_5f()
+        {
+            Assert.AreEqual(0.292893231f, Easing.ApplyEase(0.5f, EaseType.InSine));
+            Assert.AreEqual(0.707106769f, Easing.ApplyEase(0.5f, EaseType.OutSine));
+            Assert.AreEqual(0.5f, Easing.ApplyEase(0.5f, EaseType.InOutSine));
+
+            Assert.AreEqual(0.25f, Easing.ApplyEase(0.5f, EaseType.InQuad));
+            Assert.AreEqual(0.75f, Easing.ApplyEase(0.5f, EaseType.OutQuad));
+            Assert.AreEqual(0.5f, Easing.ApplyEase(0.5f, EaseType.InOutQuad));
+
+            Assert.AreEqual(0.125f, Easing.ApplyEase(0.5f, EaseType.InCubic));
+            Assert.AreEqual(0.875f, Easing.ApplyEase(0.5f, EaseType.OutCubic));
+            Assert.AreEqual(0.5f, Easing.ApplyEase(0.5f, EaseType.InOutCubic));
+
+            Assert.AreEqual(0.0625f, Easing.ApplyEase(0.5f, EaseType.InQuart));
+            Assert.AreEqual(0.9375f, Easing.ApplyEase(0.5f, EaseType.OutQuart));
+            Assert.AreEqual(0.5f, Easing.ApplyEase(0.5f, EaseType.InOutQuart));
+
+            Assert.AreEqual(0.03125f, Easing.ApplyEase(0.5f, EaseType.InQuint));
+            Assert.AreEqual(0.96875f, Easing.ApplyEase(0.5f, EaseType.OutQuint));
+            Assert.AreEqual(0.5f, Easing.ApplyEase(0.5f, EaseType.InOutQuint));
+
+            Assert.AreEqual(0.03125f, Easing.ApplyEase(0.5f, EaseType.InExpo));
+            Assert.AreEqual(0.96875f, Easing.ApplyEase(0.5f, EaseType.OutExpo));
+            Assert.AreEqual(0.5f, Easing.ApplyEase(0.5f, EaseType.InOutExpo));
+
+            Assert.AreEqual(0.133974612f, Easing.ApplyEase(0.5f, EaseType.InCirc));
+            Assert.AreEqual(0.8660254f, Easing.ApplyEase(0.5f, EaseType.OutCirc));
+            Assert.AreEqual(0.5f, Easing.ApplyEase(0.5f, EaseType.InOutCirc));
+
+            Assert.AreEqual(-0.087697506f, Easing.ApplyEase(0.5f, EaseType.InBack));
+            Assert.AreEqual(1.08769751f, Easing.ApplyEase(0.5f, EaseType.OutBack));
+            Assert.AreEqual(0.5f, Easing.ApplyEase(0.5f, EaseType.InOutBack));
+
+            Assert.AreEqual(-0.0156249879f, Easing.ApplyEase(0.5f, EaseType.InElastic));
+            Assert.AreEqual(1.015625f, Easing.ApplyEase(0.5f, EaseType.OutElastic));
+            Assert.AreEqual(0.5f, Easing.ApplyEase(0.5f, EaseType.InOutElastic));
+
+            Assert.AreEqual(0.234375f, Easing.ApplyEase(0.5f, EaseType.InBounce));
+            Assert.AreEqual(0.765625f, Easing.ApplyEase(0.5f, EaseType.OutBounce));
+            Assert.AreEqual(0.5f, Easing.ApplyEase(0.5f, EaseType.InOutBounce));
+        }
     }
 }
